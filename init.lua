c33b186fffefac76b1b94b053361689d7edb7748
Adam Gibbins <adam@adamgibbins.com>
2025-01-18 21:14:01 +0000

    Remove vim background colour to fix transparency

--- init.lua
+++ init.lua
@@ -106,3 +106,9 @@ vim.keymap.set("n", ";", ":")
 vim.keymap.set("n", "<Leader>,", "<cmd>lua require('fzf-lua').files()<CR>", { silent = true })
 vim.keymap.set("n", "<Leader>ss", "<cmd>setlocal spell!<CR>")
 vim.keymap.set("n", "<Leader>tn", "<cmd>tabnew<CR>")
+
+-- Remove background so terminal transparency works
+vim.cmd("highlight Normal ctermbg=NONE guibg=NONE")
+vim.cmd("highlight NonText ctermbg=NONE guibg=NONE")
+vim.cmd("highlight LineNr ctermbg=NONE guibg=NONE")
+vim.cmd("highlight SignColumn ctermbg=NONE guibg=NONE")
