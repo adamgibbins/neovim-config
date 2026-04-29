74cf33e5a4565e5c34c155c82ce43cd9022a19a5
Adam Gibbins <adam@adamgibbins.com>
2026-04-29 18:48:40 +0100

    Fix deprecation

--- init.lua
+++ init.lua
@@ -22,7 +22,7 @@ vim.opt.scrolloff = 20
 vim.opt.sidescrolloff = 5
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
